21a9722eac7572ec904aac40551b31128d20fc62
Youtested <youtested64@gmail.com>
2026-07-23 19:48:44 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/events/ProductViewEvent.java
@@ -0,0 +1,9 @@
+package com.distriqt.extension.inappbilling.events;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class ProductViewEvent {
+    public static final String DISMISSED = "productview:dismissed";
+    public static final String DISPLAYED = "productview:displayed";
+    public static final String FAILED = "productview:failed";
+    public static final String LOADED = "productview:loaded";
+}
