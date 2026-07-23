e1f3d92bfd2636ef5ed336f2cc00717e8d1b9c1c
Youtested <youtested64@gmail.com>
2026-07-23 19:48:30 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/util/IEventDispatcher.java
@@ -0,0 +1,6 @@
+package com.distriqt.extension.inappbilling.util;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public interface IEventDispatcher {
+    void dispatchEvent(String str, String str2);
+}
