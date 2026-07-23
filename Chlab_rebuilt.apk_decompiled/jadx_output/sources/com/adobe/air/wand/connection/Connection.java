3037065510d18b53cd35025b5aea62811f6335c4
Youtested <youtested64@gmail.com>
2026-07-23 19:47:09 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/air/wand/connection/Connection.java
@@ -0,0 +1,40 @@
+package com.adobe.air.wand.connection;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public interface Connection {
+
+    /* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+    public interface Listener {
+        void onConnectError();
+
+        void onConnectSuccess();
+
+        void onConnectionClose();
+
+        void onConnectionOpen(String str);
+
+        void onDisconnectError();
+
+        void onDisconnectSuccess();
+
+        void onReceive(String str);
+
+        void updateConnectionToken(String str);
+    }
+
+    void connect() throws Exception;
+
+    void disconnect() throws Exception;
+
+    String getConnectionToken() throws Exception;
+
+    void onConnectionChanged();
+
+    void onReadyForConnection() throws Exception;
+
+    void registerListener(Listener listener) throws Exception;
+
+    void send(String str) throws Exception;
+
+    void unregisterListener();
+}
