606d5506316f7c388a842a17d12389d0681b4aae
Youtested <youtested64@gmail.com>
2026-07-23 19:46:40 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/air/wand/motionsensor/Gyroscope.java
@@ -0,0 +1,10 @@
+package com.adobe.air.wand.motionsensor;
+
+import android.app.Activity;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class Gyroscope extends MotionSensor {
+    public Gyroscope(Activity activity) {
+        super(activity, 4);
+    }
+}
