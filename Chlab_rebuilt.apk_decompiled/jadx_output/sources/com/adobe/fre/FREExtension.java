2df08daa21106feefd05f43a4e8b4efcc314d0cc
Youtested <youtested64@gmail.com>
2026-07-23 19:48:17 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/fre/FREExtension.java
@@ -0,0 +1,10 @@
+package com.adobe.fre;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public interface FREExtension {
+    FREContext createContext(String str);
+
+    void dispose();
+
+    void initialize();
+}
