c1bb7e4cdc7350860c6d95a081c42ec929b612e8
Youtested <youtested64@gmail.com>
2026-07-23 19:48:48 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/events/ExtensionEvent.java
@@ -0,0 +1,6 @@
+package com.distriqt.extension.inappbilling.events;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class ExtensionEvent {
+    public static String ERROR = "extension:error";
+}
