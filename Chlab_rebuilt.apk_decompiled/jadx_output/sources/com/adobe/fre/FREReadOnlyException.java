d96501875a6f184a5f1fdc1bc555d8cc47a0526e
Youtested <youtested64@gmail.com>
2026-07-23 19:48:20 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/fre/FREReadOnlyException.java
@@ -0,0 +1,6 @@
+package com.adobe.fre;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class FREReadOnlyException extends Exception {
+    public static final long serialVersionUID = 1;
+}
