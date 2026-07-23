aab41bd74df372909d4c7e473674fc446dec5cb4
Youtested <youtested64@gmail.com>
2026-07-23 19:48:02 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/air/FlashEGL.java
@@ -0,0 +1,44 @@
+package com.adobe.air;
+
+import android.view.SurfaceView;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public interface FlashEGL {
+    int CreateDummySurfaceAndContext();
+
+    int CreateGLContext(boolean z);
+
+    int CreateWindowSurface(SurfaceView surfaceView, int i);
+
+    boolean DestroyGLContext();
+
+    boolean DestroyWindowSurface();
+
+    int[] GetConfigs(boolean z, boolean z2);
+
+    int[] GetNumConfigs();
+
+    int GetSurfaceHeight();
+
+    int GetSurfaceWidth();
+
+    boolean HasGLContext();
+
+    int InitEGL();
+
+    boolean IsARGBSurface();
+
+    boolean IsBufferPreserve();
+
+    boolean IsEmulator();
+
+    int MakeGLCurrent();
+
+    void ReleaseGPUResources();
+
+    void SetConfig(int i);
+
+    void SwapEGLBuffers();
+
+    void TerminateEGL();
+}
