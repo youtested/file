fe80a27a56154433e62ae3479046dad8ebc28b85
Youtested <youtested64@gmail.com>
2026-07-23 19:46:43 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/air/wand/motionsensor/Accelerometer.java
@@ -0,0 +1,10 @@
+package com.adobe.air.wand.motionsensor;
+
+import android.app.Activity;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class Accelerometer extends MotionSensor {
+    public Accelerometer(Activity activity) {
+        super(activity, 1);
+    }
+}
