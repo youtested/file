c6cee2c729beeecf464000fc627391c70699e58d
Youtested <youtested64@gmail.com>
2026-07-23 19:45:41 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/air/thix/sciencesense/chemist/GetVersionCode.java
@@ -0,0 +1,8 @@
+package air.thix.sciencesense.chemist;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class GetVersionCode {
+    public static void main(String[] args) {
+        System.out.println("5.0.3");
+    }
+}
