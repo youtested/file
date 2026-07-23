14b34b2238d101225b8c1b0fca2a753762c0d0dc
Youtested <youtested64@gmail.com>
2026-07-23 19:48:56 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/functions/purchases/RestorePurchasesFunction.java
@@ -0,0 +1,29 @@
+package com.distriqt.extension.inappbilling.functions.purchases;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREObject;
+import com.distriqt.extension.inappbilling.InAppBillingContext;
+import com.distriqt.extension.inappbilling.util.FREUtils;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class RestorePurchasesFunction implements FREFunction {
+    public static String TAG = RestorePurchasesFunction.class.getSimpleName();
+
+    @Override // com.adobe.fre.FREFunction
+    public FREObject call(FREContext context, FREObject[] args) {
+        FREUtils.log(TAG, "call", new Object[0]);
+        try {
+            InAppBillingContext ctx = (InAppBillingContext) context;
+            Boolean success = false;
+            if (ctx.v) {
+                success = Boolean.valueOf(ctx.controller().restorePurchases());
+            }
+            FREObject result = FREObject.newObject(success.booleanValue());
+            return result;
+        } catch (Exception e) {
+            FREUtils.handleException(context, e);
+            return null;
+        }
+    }
+}
