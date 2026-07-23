db09cef130f64157f99b258d4621f96c3df4878d
Youtested <youtested64@gmail.com>
2026-07-23 19:45:45 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/android/vending/billing/util/IabResult.java
@@ -0,0 +1,36 @@
+package com.android.vending.billing.util;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class IabResult {
+    String mMessage;
+    int mResponse;
+
+    public IabResult(int response, String message) {
+        this.mResponse = response;
+        if (message == null || message.trim().length() == 0) {
+            this.mMessage = IabHelper.getResponseDesc(response);
+        } else {
+            this.mMessage = message + " (response: " + IabHelper.getResponseDesc(response) + ")";
+        }
+    }
+
+    public int getResponse() {
+        return this.mResponse;
+    }
+
+    public String getMessage() {
+        return this.mMessage;
+    }
+
+    public boolean isSuccess() {
+        return this.mResponse == 0;
+    }
+
+    public boolean isFailure() {
+        return !isSuccess();
+    }
+
+    public String toString() {
+        return "IabResult: " + getMessage();
+    }
+}
