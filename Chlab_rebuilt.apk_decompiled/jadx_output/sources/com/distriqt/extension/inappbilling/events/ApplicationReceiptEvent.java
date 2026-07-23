c441014092e805c72873bc249f805cdc95786b38
Youtested <youtested64@gmail.com>
2026-07-23 19:48:46 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/events/ApplicationReceiptEvent.java
@@ -0,0 +1,7 @@
+package com.distriqt.extension.inappbilling.events;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class ApplicationReceiptEvent {
+    public static final String REFRESH_FAILED = "applicationreceipt:refresh:failed";
+    public static final String REFRESH_SUCCESS = "applicationreceipt:refresh:success";
+}
