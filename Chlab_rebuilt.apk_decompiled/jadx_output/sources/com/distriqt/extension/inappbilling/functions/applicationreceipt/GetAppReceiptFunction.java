bdb1ec1f8537dce6fbaa17da6927d60aebbff74a
Youtested <youtested64@gmail.com>
2026-07-23 19:49:01 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/functions/applicationreceipt/GetAppReceiptFunction.java
@@ -0,0 +1,13 @@
+package com.distriqt.extension.inappbilling.functions.applicationreceipt;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREObject;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class GetAppReceiptFunction implements FREFunction {
+    @Override // com.adobe.fre.FREFunction
+    public FREObject call(FREContext arg0, FREObject[] arg1) {
+        return null;
+    }
+}
