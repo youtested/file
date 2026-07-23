6fab03130e1af2fa54b64d51e2018fb44ae502b0
Youtested <youtested64@gmail.com>
2026-07-23 19:48:10 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/fre/FREBitmapData.java
@@ -0,0 +1,60 @@
+package com.adobe.fre;
+
+import com.adobe.fre.FREObject;
+import java.nio.ByteBuffer;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class FREBitmapData extends FREObject {
+    private long m_dataPointer;
+
+    public native void acquire() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native ByteBuffer getBits() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native int getHeight() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native int getLineStride32() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native int getWidth() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native boolean hasAlpha() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native void invalidateRect(int i, int i2, int i3, int i4) throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException, IllegalArgumentException;
+
+    public native boolean isInvertedY() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native boolean isPremultiplied() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    public native void release() throws FREInvalidObjectException, FREWrongThreadException, IllegalStateException;
+
+    private FREBitmapData(FREObject.CFREObjectWrapper obj) {
+        super(obj);
+    }
+
+    protected FREBitmapData(FREObject[] constructorArgs) throws FRETypeMismatchException, FREInvalidObjectException, FREASErrorException, FRENoSuchNameException, FREWrongThreadException, IllegalStateException {
+        super("flash.display.BitmapData", constructorArgs);
+    }
+
+    public static FREBitmapData newBitmapData(int width, int height, boolean transparent, Byte[] fillColor) throws FREASErrorException, FREWrongThreadException, IllegalArgumentException {
+        if (fillColor.length != 4) {
+            throw new IllegalArgumentException("fillColor has wrong length");
+        }
+        FREObject[] array = new FREObject[4];
+        array[0] = new FREObject(width);
+        array[1] = new FREObject(height);
+        array[2] = new FREObject(transparent);
+        int color = 0;
+        int signMask = -1;
+        for (int i = 0; i < 4; i++) {
+            int cShiftCount = (3 - i) * 8;
+            color |= (fillColor[i].byteValue() << cShiftCount) & signMask;
+            signMask >>>= 8;
+        }
+        array[3] = new FREObject(color);
+        try {
+            return new FREBitmapData(array);
+        } catch (FREInvalidObjectException | FRENoSuchNameException | FRETypeMismatchException e) {
+            return null;
+        }
+    }
+}
