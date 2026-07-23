5dc1f215322905440088c8a184d63f2a3e128bf8
Youtested <youtested64@gmail.com>
2026-07-23 19:48:08 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/fre/FREFunction.java
@@ -0,0 +1,6 @@
+package com.adobe.fre;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public interface FREFunction {
+    FREObject call(FREContext fREContext, FREObject[] fREObjectArr);
+}
