fe60ec9967b2fa4446fd1faa17880a63b260a92b
Youtested <youtested64@gmail.com>
2026-07-23 19:48:11 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/fre/FREASErrorException.java
@@ -0,0 +1,11 @@
+package com.adobe.fre;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class FREASErrorException extends Exception {
+    public static final long serialVersionUID = 1;
+    private FREObject m_thrownASException;
+
+    public FREObject getThrownException() {
+        return this.m_thrownASException;
+    }
+}
