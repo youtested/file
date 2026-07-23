25f678f552de030264fad7f03f05cf912c6c4734
Youtested <youtested64@gmail.com>
2026-07-23 19:49:08 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/functions/ImplementationFunction.java
@@ -0,0 +1,20 @@
+package com.distriqt.extension.inappbilling.functions;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREObject;
+import com.adobe.fre.FREWrongThreadException;
+import com.distriqt.extension.inappbilling.InAppBillingContext;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class ImplementationFunction implements FREFunction {
+    @Override // com.adobe.fre.FREFunction
+    public FREObject call(FREContext context, FREObject[] passedArgs) {
+        try {
+            FREObject result = FREObject.newObject(InAppBillingContext.IMPLEMENTATION);
+            return result;
+        } catch (FREWrongThreadException e) {
+            return null;
+        }
+    }
+}
