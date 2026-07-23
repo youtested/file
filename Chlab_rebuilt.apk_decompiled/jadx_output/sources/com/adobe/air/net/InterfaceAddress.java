9f79be50cc02b92a4648145172eb5caae623354b
Youtested <youtested64@gmail.com>
2026-07-23 19:47:55 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/adobe/air/net/InterfaceAddress.java
@@ -0,0 +1,11 @@
+package com.adobe.air.net;
+
+import com.distriqt.extension.inappbilling.BuildConfig;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class InterfaceAddress {
+    public String address = BuildConfig.FLAVOR;
+    public String broadcast = BuildConfig.FLAVOR;
+    public String ipVersion = "IPv4";
+    public int prefixLength = -1;
+}
