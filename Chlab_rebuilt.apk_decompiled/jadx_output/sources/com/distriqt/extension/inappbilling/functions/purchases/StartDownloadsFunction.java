28b3463f1e102e30b9453421b94c20711f2f6a71
Youtested <youtested64@gmail.com>
2026-07-23 19:48:52 +0300

    Upload Chlab_rebuilt.apk_decompiled

---
+++ Chlab_rebuilt.apk_decompiled/jadx_output/sources/com/distriqt/extension/inappbilling/functions/purchases/StartDownloadsFunction.java
@@ -0,0 +1,28 @@
+package com.distriqt.extension.inappbilling.functions.purchases;
+
+import com.adobe.fre.FREContext;
+import com.adobe.fre.FREFunction;
+import com.adobe.fre.FREObject;
+import com.distriqt.extension.inappbilling.InAppBillingContext;
+import com.distriqt.extension.inappbilling.util.FREUtils;
+
+/* loaded from: /home/codespace/chem/Chlab_rebuilt.apk_decompiled/build/apk/classes.dex */
+public class StartDownloadsFunction implements FREFunction {
+    public static String TAG = StartDownloadsFunction.class.getSimpleName();
+
+    @Override // com.adobe.fre.FREFunction
+    public FREObject call(FREContext context, FREObject[] args) {
+        FREUtils.log(TAG, "call", new Object[0]);
+        try {
+            InAppBillingContext ctx = (InAppBillingContext) context;
+            Boolean success = false;
+            if (ctx.v) {
+            }
+            FREObject result = FREObject.newObject(success.booleanValue());
+            return result;
+        } catch (Exception e) {
+            FREUtils.handleException(context, e);
+            return null;
+        }
+    }
+}
